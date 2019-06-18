fe4d5e953d3b79957c8828cfb1cccb303554102b
WebFlat <y.vacheslav@gmail.com>
2019-06-19 00:58:26 +0300

    final

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         dots: true,
         slidesToShow: 3,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 2000
 
   });
 
@@ -35,8 +35,184 @@ function initMap() {
     lng: -73.896072
   }
   let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: geo
+    zoom: 13,
+    center: geo,
+     styles: [{
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#f5f5f5"
+         }]
+       },
+       {
+         "elementType": "labels.icon",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#616161"
+         }]
+       },
+       {
+         "elementType": "labels.text.stroke",
+         "stylers": [{
+           "color": "#f5f5f5"
+         }]
+       },
+       {
+         "featureType": "administrative.land_parcel",
+         "elementType": "labels",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "administrative.land_parcel",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#bdbdbd"
+         }]
+       },
+       {
+         "featureType": "poi",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#eeeeee"
+         }]
+       },
+       {
+         "featureType": "poi",
+         "elementType": "labels.text",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "poi",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#757575"
+         }]
+       },
+       {
+         "featureType": "poi.business",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "poi.park",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#e5e5e5"
+         }]
+       },
+       {
+         "featureType": "poi.park",
+         "elementType": "labels.text",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "poi.park",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#9e9e9e"
+         }]
+       },
+       {
+         "featureType": "road",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#ffffff"
+         }]
+       },
+       {
+         "featureType": "road.arterial",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "road.arterial",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#757575"
+         }]
+       },
+       {
+         "featureType": "road.highway",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#dadada"
+         }]
+       },
+       {
+         "featureType": "road.highway",
+         "elementType": "labels",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "road.highway",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#616161"
+         }]
+       },
+       {
+         "featureType": "road.local",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "road.local",
+         "elementType": "labels",
+         "stylers": [{
+           "visibility": "off"
+         }]
+       },
+       {
+         "featureType": "road.local",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#9e9e9e"
+         }]
+       },
+       {
+         "featureType": "transit.line",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#e5e5e5"
+         }]
+       },
+       {
+         "featureType": "transit.station",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#eeeeee"
+         }]
+       },
+       {
+         "featureType": "water",
+         "elementType": "geometry",
+         "stylers": [{
+           "color": "#c9c9c9"
+         }]
+       },
+       {
+         "featureType": "water",
+         "elementType": "labels.text.fill",
+         "stylers": [{
+           "color": "#9e9e9e"
+         }]
+       }
+     ]
   });
 
   let image = '../img/marker.png';
@@ -46,4 +222,4 @@ function initMap() {
     icon: image,
     title: '91 Nolan'
   });
-};
+}
